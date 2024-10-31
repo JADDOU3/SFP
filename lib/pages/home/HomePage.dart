7a11b578fba75a3f711a0de12cd940cf6b0845c0
JADDOU3 <22the7@gmail.com>
2024-11-01 00:33:28 +0200

    Popular Diets implementation

--- lib/pages/home/HomePage.dart
+++ lib/pages/home/HomePage.dart
@@ -5,15 +5,18 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test_project/models/CategoryModel.dart';
 import 'package:flutter_test_project/models/DietModel.dart';
+import 'package:flutter_test_project/models/PopularDietsModel.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
   List<CategoryModel> categories = [];
   List<DietModel> diets = [];
+  List<PopularDietsModel> popular = [];
 
   void getInetialInfo(){
     categories = CategoryModel.getCategory();
     diets = DietModel.getDiets();
+    popular = PopularDietsModel.getPopularDiets();
   }
 
   @override
@@ -29,6 +32,8 @@ class HomePage extends StatelessWidget {
           categorySection(),
           SizedBox(height: 40,),
           dietSection(),
+          SizedBox(height: 40,),
+          popularDietSection(),
         ],
       ),
     );
@@ -297,6 +302,93 @@ class HomePage extends StatelessWidget {
               left: 20
             ),
           ),
+        )
+      ],
+    );
+  }
+
+  Column popularDietSection(){
+    return   Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Text(
+            'Popular',
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.w600
+            ),
+          ),
+        ),
+        const SizedBox(height: 15,),
+        ListView.separated(
+          itemCount: popular.length,
+          shrinkWrap: true,
+          separatorBuilder: (context, index) => const SizedBox(height: 25,),
+          padding: const EdgeInsets.only(
+              left: 20,
+              right: 20
+          ),
+          itemBuilder: (context, index) {
+            return Container(
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SvgPicture.asset(
+                    popular[index].iconPath,
+                    width: 65,
+                    height: 65,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        popular[index].name,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
+                            fontSize: 16
+                        ),
+                      ),
+                      Text(
+                        popular[index].level + ' | ' + popular[index].duration + ' | ' + popular[index].calorie,
+                        style: const TextStyle(
+                            color: Color(0xff7B6F72),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400
+                        ),
+                      ),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: (){},
+                    child: SvgPicture.asset(
+                      'assets/icons/button.svg',
+                      width: 30,
+                      height: 30,
+                    ),
+                  )
+                ],
+              ),
+              decoration: BoxDecoration(
+                  color: popular[index].boxIsSelected ?
+                  Colors.white : Colors.transparent,
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: popular[index].boxIsSelected ? [
+                    BoxShadow(
+                        color: const Color(0xff1D1617).withOpacity(0.07),
+                        offset: const Offset(0, 10),
+                        blurRadius: 40,
+                        spreadRadius: 0
+                    )
+                  ] : []
+              ),
+            );
+          },
         )
       ],
     );
